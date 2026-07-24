4f4f2392eb59e5cda9c5c79eb5a78170d4f4e1b5
Saleh Abbasov <sabbasov26376@ada.edu.az>
2026-07-25 00:20:18 +0400

    Add Tasks

---
+++ Lab3 newproject/Lab3 newproject/Program.cs
@@ -0,0 +1,186 @@
+﻿// Console.Write("Mehsul sayi daxil et: ");
+// string input = Console.ReadLine();
+// if (!int.TryParse(input, out int productCount))
+// {
+//     Console.WriteLine("Invalid input");
+//     return;
+// }
+// string[] products = new string[productCount];
+// int[] counts = new int[productCount];
+//
+// for (int i = 0; i < productCount; i++)
+// {
+//     Console.WriteLine($"Enter product {i + 1} name:");
+//     products[i] = Console.ReadLine();
+//     Console.WriteLine($"Enter product {i + 1} amount:");
+//     string input2 = Console.ReadLine();
+//     if (!int.TryParse(input2, out int productAmount)|| productAmount < 0)
+//     {
+//         Console.WriteLine("Invalid input");
+//         return;
+//     }
+//     counts[i] = productAmount;
+// }
+//
+// while (true)
+// {
+//     
+//     Console.WriteLine("\n===== MENYU =====");
+//     Console.WriteLine("1. Bütün məhsulları göstər");
+//     Console.WriteLine("2. Ümumi məhsul sayını göstər");
+//     Console.WriteLine("3. Ən çox olan məhsulu göstər");
+//     Console.WriteLine("4. Ən az olan məhsulu göstər");
+//     Console.WriteLine("5. Müəyyən saydan çox olan məhsulları göstər");
+//     Console.WriteLine("6. Məhsul axtar");
+//     Console.WriteLine("7. Yeni məhsul sayı əlavə et");
+//     Console.WriteLine("8. Çıxış");
+//     Console.Write("Seciminizi daxil edin (1-8): ");
+//     string choice = Console.ReadLine();
+//     switch (choice)
+//     {
+//         case "1":
+//             ShowProducts(products,counts);
+//             break;
+//         case "2":
+//             Console.WriteLine((GetTotalCount(counts)));
+//             break;
+//         case "3":
+//             GetMaxProduct(counts);
+//             break;
+//         case "4":
+//             GetMinProduct(counts);
+//             break;
+//         case "5":
+//             Console.Write("Say daxil edin: ");
+//             int number = int.Parse(Console.ReadLine());
+//             ShowProductsGreaterThan(products,counts,number);
+//             break;
+//         case "6":
+//             Console.Write("Mehsulun adini daxil edin: ");
+//             string name = Console.ReadLine();
+//             SearchProduct(products,counts,name);
+//             break;
+//         case "7":
+//             Console.Write("Mehsulun adini daxil edin: ");
+//             string productName = Console.ReadLine();
+//             Console.Write("Say daxil edin: ");
+//             int amount = int.Parse(Console.ReadLine());
+//             AddCount(products,counts,productName,amount);
+//             break;
+//         case "8":
+//             Console.WriteLine("Program baglandi.");
+//             break;
+//         default:
+//             Console.WriteLine("Error!");
+//             break;
+//     }
+//    
+//     
+// }
+//
+// void ShowProducts(string[] products, int[] counts)
+// {
+//     for (int i = 0; i < products.Length; i++)
+//     {
+//         Console.WriteLine($"{products[i]}: {counts[i]}");
+//     }
+// }
+//
+// int GetTotalCount(int[] counts)
+// {
+//     int total = 0;
+//     for (int i = 0; i < counts.Length; i++)
+//     {
+//         total+=counts[i];
+//     }
+//     return total;
+// }
+//
+// void GetMaxProduct(int[] counts)
+// {
+//     int max = counts[0];
+//     int index = 0;
+//     for (int i = 0; i < counts.Length; i++)
+//     {
+//         if (counts[i] > max)
+//         {
+//             max = counts[i];
+//             index = i;
+//         }
+//     }
+//     Console.WriteLine($"{products[index]}: {max}");
+// }
+//
+// void GetMinProduct(int[] counts)
+// {
+//     int min = counts[0];
+//     int index = 0;
+//     for (int i = 0; i < counts.Length; i++)
+//     {
+//         if (counts[i] < min)
+//         {
+//             min = counts[i];
+//             index = i;
+//         }
+//     }
+//     Console.WriteLine($"{products[index]}: {min}");
+// }
+//
+// void ShowProductsGreaterThan(string[] products, int[] counts, int number)
+// {
+//     bool found = false;
+//     for (int i = 0; i < products.Length; i++)
+//     {
+//         if (counts[i] > number)
+//         {
+//             Console.WriteLine($"{products[i]}: {counts[i]}");
+//             found = true;
+//         }
+//     }
+//
+//     if (!found)
+//     {
+//         Console.WriteLine("Mehsul tapilmadi. ");
+//     }
+// }
+//
+// void SearchProduct(string[] products, int[] counts, string name)
+// {
+//     bool found = false;
+//     for (int i = 0; i < products.Length; i++)
+//     {
+//         if (products[i].ToLower() == name.ToLower())
+//         {
+//             found = true;
+//             break;
+//         }
+//     }
+//
+//     if (!found)
+//     {
+//         Console.WriteLine("Mehsul tapilmadi. ");
+//     }
+// }
+//
+// void AddCount(string[] products, int[] counts, string name, int amount)
+// {
+//     bool found = false;
+//     for (int i = 0; i < products.Length; i++)
+//     {
+//         if (products[i].ToLower() == name.ToLower())
+//         {
+//             counts[i]+=amount;
+//             Console.WriteLine($"Yeni say: {counts[i]}");
+//             found = true;
+//             break;
+//         }
+//     }
+//     if (!found)
+//     {
+//         Console.WriteLine("Mehsul tapilmadi. ");
+//     }
+// }
+
+
+
+
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,155 @@
+﻿// Console.Write("Telebe sayini daxil et: ");
+// string input = Console.ReadLine();
+// if (!int.TryParse(input, out int studentCount))
+// {
+//     Console.WriteLine("Telebe sayiniz bulunamadi");
+//     return;
+// }
+// string[] students = new string[studentCount];
+// int[]grades = new int[studentCount];
+//
+// for (int i = 0; i < studentCount; i++)
+// {
+//     Console.Write($"Enter student {i+1} name : ");
+//     students[i] = Console.ReadLine();
+//     Console.Write("Telebenin balini daxil edin: ");
+//     string input2 = Console.ReadLine();
+//     if (!int.TryParse(input2, out int studentGrade)|| studentGrade<0 || studentGrade>100)
+//     {
+//         Console.WriteLine("Invalid input");
+//         return;
+//     }
+//     grades[i] = studentGrade;
+// }
+//
+// while (true)
+// {
+//     Console.WriteLine("\n===== MENYU =====");
+//     Console.WriteLine("1. Bütün tələbələri göstər");
+//     Console.WriteLine("2. Orta balı göstər");
+//     Console.WriteLine("3. Ən yüksək balı göstər");
+//     Console.WriteLine("4. Ən aşağı balı göstər");
+//     Console.WriteLine("5. Keçən tələbələri göstər");
+//     Console.WriteLine("6. Kəsilən tələbələri göstər");
+//     Console.WriteLine("7. Ada görə tələbə axtar");
+//     Console.WriteLine("8. Çıxış");
+//     Console.Write("Seciminizi daxil edin (1-8): ");
+//     string choice = Console.ReadLine();
+//     switch (choice)
+//     {
+//         case "1":
+//             ShowAllStudents(students, grades);
+//             break;
+//         case "2":
+//             Console.WriteLine("Average is: " + Average(grades));
+//             break;
+//         case "3":
+//             Console.WriteLine("Max is: " + MaxGrade(grades));
+//             break;
+//         case "4":
+//             Console.WriteLine("Min is: " + MinGrade(grades));
+//             break;
+//         case "5":
+//             ShowPassed(students, grades);
+//             break;
+//         case "6":
+//             ShowFailed(students, grades);
+//             break;
+//         case "7":
+//             SearchStudent(students, grades);
+//             break;
+//         case "8":
+//         {
+//             Console.Write("Program baglandi. ");
+//             break;
+//         }
+//         
+//     }
+// }
+//
+// void ShowAllStudents(string[] students, int[] grades)
+// {
+//     for (int i = 0; i < students.Length; i++)
+//     {
+//         Console.WriteLine($"{students[i]} - {grades[i]}");
+//     }
+// }
+//
+// double Average(int[] grades)
+// {
+//     double sum = 0;
+//     foreach (int grade in grades)
+//     {
+//         sum += grade;
+//     }
+//     return sum / grades.Length;
+//     
+// }
+//
+//
+//
+// int MaxGrade(int[] grades)
+// {
+//     int max = grades[0];
+//     foreach (int grade in grades)
+//     {
+//         if (grade > max)
+//         {
+//             max = grade;
+//         }
+//     }
+//     return max;
+// }
+//
+// int MinGrade(int[] grades)
+// {
+//     int min= grades[0];
+//     foreach (int grade in grades)
+//     {
+//         if (grade < min)
+//         {
+//             min = grade;
+//         }
+//     }
+//     return min;
+// }
+//
+//  void ShowPassed(string[]students, int[] grades)
+// {
+//     Console.Write("\nKecen telebeler: ");
+//     for (int i = 0; i < students.Length; i++)
+//     {
+//         if (grades[i] >= 60)
+//         {
+//             Console.WriteLine($"{students[i]} - {grades[i]}");
+//         }
+//     }
+// }
+//  void ShowFailed(string[] students, int[] grades)
+// {
+//     Console.WriteLine("\nKəsilən tələbələr:");
+//
+//     for (int i = 0; i < students.Length; i++)
+//     {
+//         if (grades[i] < 60)
+//         {
+//             Console.WriteLine($"{students[i]} - {grades[i]}");
+//         }
+//     }
+// }
+//
+//  void SearchStudent(string[] students, int[] grades)
+// {
+//     Console.Write("Axtarmaq istediyiniz telebenin adini qeyd edin: ");
+//     string name = Console.ReadLine();
+//     for (int i=0;i<students.Length;i++)
+//     {
+//         if (students[i] == name)
+//         {
+//            Console.WriteLine($"Student name: {students[i]},Grade: {grades[i]}");
+//            return;
+//         }
+//     }
+//     Console.Write("Student not found!");
+// }
+
